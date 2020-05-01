ff0aabd68008bb45fbb7351271960ae4b8d04390
Mason Burke <masonburke@rocketmail.com>
2020-04-30 22:57:07 -0600

    groundwork is done

---
+++ Eat-Da-Burger/db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS Eat_da_burger_db;
+CREATE DATABASE Eat_da_burger_db;
+USE Eat_da_burger_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ Eat-Da-Burger/db/seeds.sql
@@ -0,0 +1,8 @@
+
+
+INSERT INTO burgers (name) VALUES ("veggie burger");
+INSERT INTO burgers (name) VALUES ("impossible whopper");
+INSERT INTO burgers (name, devoured) VALUES ("black bean burger", true);
+INSERT INTO burgers (name, devoured) VALUES ("lentil burger", true);
+INSERT INTO burgers (name, devoured) VALUES ("follow your heart burger", true);
+INSERT INTO burgers (name) VALUES ("beyond meat burger");
